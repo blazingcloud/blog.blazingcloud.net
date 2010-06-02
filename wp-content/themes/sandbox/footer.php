d8cb597355984bcdf907f50e2ac7ad2b1b24f06a
Liah Hansen <liahhansen@gmail.com>
2010-06-02 15:59:48 -0700

    adding sandbox theme

---
+++ wp-content/themes/sandbox/footer.php
@@ -0,0 +1,11 @@
+
+	<div id="footer">
+	Copyright 2010, Blazing Cloud LLC
+	</div><!-- #footer -->
+
+</div><!-- #wrapper .hfeed -->
+
+<?php wp_footer() ?>
+
+</body>
+</html>
---
+++ wp-content/themes/sandbox/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
+<head profile="http://gmpg.org/xfn/11">
+	<title><?php wp_title( '-', true, 'right' ); echo wp_specialchars( get_bloginfo('name'), 1 ) ?></title>
+	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>" />
+<?php wp_head() // For plugins ?>
+	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php printf( __( '%s latest posts', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
+	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
+</head>
+
+<body class="<?php sandbox_body_class() ?>">
+
+<div id="wrapper" class="hfeed">
+
+	<div id="header">
+		<a href="<?php bloginfo('url'); ?>">
+			<img src='wp-content/themes/sandbox/images/logo.png' alt='Blazing Cloud' />
+		</a>
+	</div><!--  #header -->
+
+	<div id="access">
+		<?php sandbox_globalnav() ?>
+	</div><!-- #access -->
+	
+	<div id="cur-date">
+		<?php echo date("F j, Y"); ?>
+	</div>
