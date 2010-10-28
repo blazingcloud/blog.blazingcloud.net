f1c003aa7303c2b7964cd4d1938ad19ca53c54c5
Lee Lundrigan <lee.lundrigan@gmail.com>
2010-10-28 12:19:31 -0700

    made page be the same as page with no title

--- wp-content/themes/sandbox/page.php
+++ wp-content/themes/sandbox/page.php
@@ -1,26 +1,48 @@
+<?php
+/*
+Page Without a Title
+*/
+?>
+
 <?php get_header() ?>
 
 <div id="content-wrapper">
-    <div id="side-bar">           
-        <?php get_sidebar() ?>
+    <div id="side-bar">       
+        <?php
+        #FIXME: isn't there a better way to get portfolio's page id???
+        $page_title = trim(wp_title( NULL, 0, NULL));
+        $page_ID = -1;
+        if($page_title == "Portfolio") {
+            foreach(get_pages() as $page) {
+                $page_ID = $page->ID;
+                if(get_the_title($page_ID) == $page_title) {
+                    break;
+                }
+            }
+        }
+                                
+        if($page_ID != -1) {
+            get_portfolio($page_ID);
+        } else {
+            get_sidebar();
+        }
+        ?>
     </div><!-- side-bar -->
             
     <div id="main-content">
     
-            <?php the_post() ?>
-
-			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
-				<h2 class="entry-title"><?php the_title() ?></h2>
-				<div class="entry-content">
+        <?php the_post() ?>
+        <div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
+            <div class="entry-content">
                 <?php the_content() ?>
-                
+    
                 <?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'sandbox' ) . '&after=</div>') ?>
-                
+    
                 <?php edit_post_link( __( 'Edit', 'sandbox' ), '<span class="edit-link">', '</span>' ) ?>
-
-				</div>
-			</div><!-- .post -->
-
+    
+            </div>
+        </div><!-- .post -->
+    
         <?php if ( get_post_custom_values('comments') ) comments_template() // Add a key+value of "comments" to enable comments on this page ?>
     
         <div id="footer">
