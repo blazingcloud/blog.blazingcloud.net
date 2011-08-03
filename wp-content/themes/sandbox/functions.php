0bbdc34618283747d97d10e9b40f93fefabe1673
root <root@ultrasaurus.com>
2011-08-03 15:40:22 -0700

    changes classes link

--- wp-content/themes/sandbox/functions.php
+++ wp-content/themes/sandbox/functions.php
@@ -53,7 +53,7 @@ function blazingcloud_globalnav() {
 	$url = get_bloginfo('url');
 	$pages = array("Home" => $url, 
 	               "Portfolio" => $url . "/portfolio", 
-	               "Classes" => "http://classes.blazingcloud.net", 
+	               "Classes" => "http://blazingcloud.eventbrite.com", 
 	               "Cross Training" => $url . "/crosstrain", 
 	               "Contact Us" => $url . "/contact-us", 
 	               "Blog" => $url . "/blog");
