a5a2f4106a315d5965918a178d039354f64a2494
Hollin Wilkins <chromaticbum@gmail.com>
2012-03-20 11:57:44 -0700

    Working on new theme to match new website.

---
+++ wp-content/themes/shortgrain/404.php
@@ -0,0 +1,31 @@
+<?php get_header() ?>
+
+<div id="content-wrapper">
+    <div id="side-bar">           
+        <?php get_sidebar() ?>
+    </div><!-- side-bar -->
+            
+    <div id="main-content">
+
+
+
+			<div id="post-0" class="post error404 not-found">
+				<h2 class="entry-title"><?php _e( 'Not Found', 'sandbox' ) ?></h2>
+				<div class="entry-content">
+					<p><?php _e( 'Apologies, but we were unable to find what you were looking for. Perhaps  searching will help.', 'sandbox' ) ?></p>
+				</div>
+				<form id="searchform-404" class="blog-search" method="get" action="<?php bloginfo('home') ?>">
+					<div>
+						<input id="s-404" name="s" class="text" type="text" value="<?php the_search_query() ?>" size="40" />
+						<input class="button" type="submit" value="<?php _e( 'Find', 'sandbox' ) ?>" />
+					</div>
+				</form>
+			</div><!-- .post -->
+
+        <div id="footer">
+            <p>Copyright 2010-2011, Blazing Cloud, Inc.</p>
+        </div>
+    </div> <!-- #main-content -->
+</div><!-- #content-wrapper -->
+
+<?php get_footer() ?>
---
+++ wp-content/themes/shortgrain/footer.php
@@ -0,0 +1,25 @@
+  </section>
+
+    <footer>
+      <section class='bottom grid'>
+        <div class='bottom-logo' src='images/services/services_mobile_logo.png' alt='blazing cloud'/>
+        <section>
+          <article class='tile'>
+            <em class='title'> Contact </em>
+            <ol>
+              <li>
+                <abbr> E </abbr> info@blazingcloud.net
+              </li>
+              <li>
+                <abbr> P </abbr> (415) 800 8085
+              </li>
+              <li>
+                <abbr> F </abbr> (415) 738 5407
+              </li>
+            </ol>
+          </article>
+        </section>
+      </section>
+    </footer>
+  </body>
+</html>
