df7067e29b0de4ecbd676c2d5a49076df12c2e47
Curtis Schofield <curtis@ram9.cc>
2012-04-10 13:58:47 -0700

    single page in inedx

--- wp-content/themes/la-nube/index.php
+++ wp-content/themes/la-nube/index.php
@@ -29,14 +29,15 @@
 					<span class="author vcard"><?php printf( __( 'By %s', 'sandbox' ), '<a class="url fn n" href="' . get_author_link( false, $authordata->ID, $authordata->user_nicename ) . '" title="' . sprintf( __( 'View all posts by %s', 'sandbox' ), $authordata->display_name ) . '">' . get_the_author() . '</a>' ) ?></span>
 					<span class="meta-sep">|</span>
 					<span class="category-links"><?php printf( __( 'Posted in %s', 'sandbox' ), get_the_category_list(', ') ) ?></span>
-					<span class="meta-sep">|</span>
+          <span class="meta-sep">|</span>
+
 					<?php the_tags( __( '<span class="tag-links">Tagged ', 'sandbox' ), ", ", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
 <?php edit_post_link( __( 'Edit', 'sandbox' ), "\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
 					<span class="comments-link"><?php comments_popup_link( __( 'Comments (0)', 'sandbox' ), __( 'Comments (1)', 'sandbox' ), __( 'Comments (%)', 'sandbox' ) ) ?></span>
 				</div>
 			<nav id="nav-below" class="archives">
-				<div class="nav-previous"><?php next_post_link(__( '<span class="meta-nav">&lt;</span> Previous', 'sandbox' )) ?></div>
-				<div class="nav-next"><?php previous_post_link(__( 'Next <span class="meta-nav">&gt;</span>', 'sandbox' )) ?></div>
+				<div class="nav-previous"><?php previous_post_link('%link',__( '<span class="meta-nav">&lt;</span> Previous', 'sandbox' ),TRUE) ?></div>
+				<div class="nav-next"><?php next_post_link('%link',__( 'Next <span class="meta-nav">&gt;</span>', 'sandbox' ),TRUE) ?></div>
 			</nav>
        <?php comments_template() ?>
 			</div><!-- .post -->
