0e6cff3a1d434456310a2d3e36b487bce0b1fe28
Curtis Schofield <curtis@ram9.cc>
2012-04-10 13:21:08 -0700

    basics of responsive

--- wp-content/themes/la-nube/index.php
+++ wp-content/themes/la-nube/index.php
@@ -1,45 +1,51 @@
 <?php get_header() ?>
-<div id="content-wrapper blog">
+<div class="blog">
             
-    <div id="main-content">
+    <div id="main-content" class='grid'>
 
-			<nav id="nav-above" class="navigation">
-				<div class="nav-previous"><?php next_posts_link(__( '<span class="meta-nav">&lt;</span> Previous', 'sandbox' )) ?></div>
-				<div class="nav-next"><?php previous_posts_link(__( 'Next <span class="meta-nav">&gt;</span>', 'sandbox' )) ?></div>
-			</nav>
 
-<?php while ( have_posts() ) : the_post() ?>
+<?php  the_post() ?>
 
-			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
-				<h1 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( __('Permalink to %s', 'sandbox'), the_title_attribute('echo=0') ) ?>" rel="bookmark"><?php the_title() ?></a></h1>
-				<div class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php unset($previousday); printf( __( '%1$s &#8211; %2$s', 'sandbox' ), the_date( '', '', '', false ), get_the_time() ) ?></abbr></div>
-				<div class="entry-content">
+			<div id="post-<?php the_ID() ?>" class="tile entry <?php sandbox_post_class() ?>">
+				<h1><a class='title' href="<?php the_permalink() ?>" title="<?php printf( __('Permalink to %s', 'sandbox'), the_title_attribute('echo=0') ) ?>" rel="bookmark"><?php the_title() ?></a></h1>
+				<div class="meta">
+					<span class="author vcard"><?php printf( __( 'By %s', 'sandbox' ), '<a class="url fn n" href="' . get_author_link( false, $authordata->ID, $authordata->user_nicename ) . '" title="' . sprintf( __( 'View all posts by %s', 'sandbox' ), $authordata->display_name ) . '">' . get_the_author() . '</a>' ) ?></span>
+					<span class="category-links"><?php printf( __( 'in %s.', 'sandbox' ), get_the_category_list(', ') ) ?></span>
+          <span>
+            Posted on
+            <abbr class="date-published" 
+                title="<?php the_time('Y-m-d\TH:i:sO') ?>">
+                <?php the_time('F jS') ?>
+            </abbr>
+          </span>
+        </div>
+
+				<div class="content">
 <?php the_content( __( 'Read More <span class="meta-nav">&raquo;</span>', 'sandbox' ) ) ?>
 
 				<?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'sandbox' ) . '&after=</div>') ?>
 				</div>
-				<div class="entry-meta">
+				<div class="meta">
 					<span class="author vcard"><?php printf( __( 'By %s', 'sandbox' ), '<a class="url fn n" href="' . get_author_link( false, $authordata->ID, $authordata->user_nicename ) . '" title="' . sprintf( __( 'View all posts by %s', 'sandbox' ), $authordata->display_name ) . '">' . get_the_author() . '</a>' ) ?></span>
 					<span class="meta-sep">|</span>
-					<span class="cat-links"><?php printf( __( 'Posted in %s', 'sandbox' ), get_the_category_list(', ') ) ?></span>
+					<span class="category-links"><?php printf( __( 'Posted in %s', 'sandbox' ), get_the_category_list(', ') ) ?></span>
 					<span class="meta-sep">|</span>
 					<?php the_tags( __( '<span class="tag-links">Tagged ', 'sandbox' ), ", ", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
 <?php edit_post_link( __( 'Edit', 'sandbox' ), "\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n" ) ?>
 					<span class="comments-link"><?php comments_popup_link( __( 'Comments (0)', 'sandbox' ), __( 'Comments (1)', 'sandbox' ), __( 'Comments (%)', 'sandbox' ) ) ?></span>
 				</div>
-			</div><!-- .post -->
-
-<?php comments_template() ?>
-
-<?php endwhile; ?>
-    <aside id="side-bar">           
-        <?php get_sidebar() ?>
-    </aside><!-- side-bar -->
-
-			<nav id="nav-below" class="navigation">
+			<nav id="nav-below" class="archives">
 				<div class="nav-previous"><?php next_posts_link(__( '<span class="meta-nav">&lt;</span> Previous', 'sandbox' )) ?></div>
 				<div class="nav-next"><?php previous_posts_link(__( 'Next <span class="meta-nav">&gt;</span>', 'sandbox' )) ?></div>
 			</nav>
+       <?php comments_template() ?>
+			</div><!-- .post -->
+
+
+    <aside id="side-bar" class='tile'>           
+        <?php get_sidebar() ?>
+    </aside><!-- side-bar -->
+
     </div> <!-- #main-content -->
 </div><!-- #content-wrapper -->
 
